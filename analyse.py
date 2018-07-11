df21ce9a7649d8597c38bc1d3ef4b96e50cefdcb
sylvie <wisepoison@gmail.com>
2018-07-11 15:57:51 -0400

    debugging

--- analyse.py
+++ analyse.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 from collections import Counter, defaultdict
 
@@ -15,24 +15,22 @@ def analyse(data):
 	all_days = []
 	weeks = defaultdict(lambda :Counter())
 	months = defaultdict(lambda :Counter())
-	for day in days
-		start = datetime.strptime(day["start"].date"%Y-%m-%d:T%H:%M:%S"
-		stop = datetime.strptime(day["stop"].date"%Y-%m-%d:T%H:%M:%S"
-		if start.hour < 20:
-			# must have gone to bed past midnight
+	for day in data:
+		start = datetime.strptime(day["start"], "%Y-%m-%dT%H:%M:%S")
+		stop = datetime.strptime(day["stop"], "%Y-%m-%dT%H:%M:%S")
+		if start.hour < 20:	# must have gone to bed past midnight
 			start_hours = 4 + start.hour + start.minute / 60.0
-			start = start.replace(day=start.day - 1)
+			start = start - time5delta(1)
 		else:
-			start_miuntes = start.hour - 20  + start.minute / 60.0
-			date = start.strftime("%Y-%m-%d")
-	    duration_hours = (stop - start).total_seconds() / 3600
-	    classification = classify(start_minutes, duration_hours)
-		all_days.append([date.strptime("%Y-%m-%d"), classification])
-		months[date.strptime("%Y-%m")].update(classification)
+			start_minutes = start.hour - 20  + start.minute / 60.0
+		duration_hours = (stop - start).total_seconds() / 3600
+		classification = classify(start_minutes, duration_hours)
+		all_days.append([start.strftime("%Y-%m-%d"), classification])
+		months[start.strftime("%Y-%m")].update([classification])
 		weeks[
-			date.strptime("%Y-%m week #") + str(date.day / 7)
-			].update(classification)
-		return all_days, months, weeks
+			start.strftime("%Y-%m week #") + str(start.day / 7)
+			].update([classification])
+	return all_days, months, weeks
 
 	
 with open("sleep_data.json") as f:
